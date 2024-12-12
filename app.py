d24c6e26e2e380aaef02a0e2519f443d9d0bd275
qkccyu <156665656+qkccyu@users.noreply.github.com>
2024-12-12 22:38:33 +0800

    app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+
+@st.cache_resource
+def load_model():
+    model = tf.keras.models.load_model('plant_classifier.hdf5')
+    return model
+
+model = load_model()
+
+st.write("""
+# Plant Leaf Detection System
+""")
+
+file = st.file_uploader("Choose plant photo from computer", type=["jpg", "png"])
+
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    if img.ndim == 2:  # Grayscale image
+        img = np.stack((img,) * 3, axis=-1)  # Convert to RGB
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
+    return prediction
+
+if file is None:
+    st.text("Please upload an image file")
+else:
+    try:
+        image = Image.open(file)
+        st.image(image, use_column_width=True)
+        prediction = import_and_predict(image, model)
+        
+        class_names = [
+            'Rasna', 'Arive-Dantu', 'Jackfruit', 'Neem', 'Basale',
+            'Indian Mustard', 'Karanda', 'Lemon', 'Roxburgh fig', 'Peepal Tree',
+            'Hibiscus', 'Jasmine', 'Mango', 'Mint', 'Drumstick',
+            'Jamaica Cherry', 'Curry Leaf', 'Oleander', 'Parijata', 'Tulsi',
+            'Betel', 'Mexican Mint', 'Indian Beech', 'Guava', 'Pomegranate',
+            'Sandalwood', 'Jamun', 'Rose Apple', 'Crape Jasmine', 'Fenugreek'
+        ]
+        
+        string = "OUTPUT : " + class_names[np.argmax(prediction)]
+        st.success(string)
+    except Exception as e:
+        st.error(f"Error processing the image: {e}")
